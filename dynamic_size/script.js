b107dd73048b27e4ed88f12b3fa0b822de0275b2
Lucas Tostes <lucas.tostes@99app.com>
2020-10-20 15:22:23 -0300

    added dynamic size subpage

---
+++ dynamic_size/script.js
@@ -0,0 +1,5 @@
+// yanked from http://thecodeplayer.com/walkthrough/css3-family-tree
+// and turned into scss
+
+// Warning: The above site is super sketchy with random page navigation, click bombs, etc. 
+// Browse with care. \
